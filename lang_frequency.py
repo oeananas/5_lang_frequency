8005a758f4d670c33ac0486a9ca2f70b70ca4f3b
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-02 19:48:48 +0300

    Update lang_frequency.py
    Добавлены функции для чтения файла и подсчета самых часто встречающихся слов в тексте

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,25 @@
+import collections
+import re
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
+        content = file.read().lower()
+        return content
 
 
 def get_most_frequent_words(text):
-    pass
+    qt_words = collections.Counter(re.findall(r'\w+', text))
+    return qt_words.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        exit("Need input file path as parameter")
+    try:
+        most_frequent_words_lst = get_most_frequent_words(load_data(sys.argv[1]))
+        for words_and_counts in most_frequent_words_lst:
+            print(words_and_counts[0], ": ", words_and_counts[1], sep='')
+    except FileNotFoundError:
+        exit("File not found")
