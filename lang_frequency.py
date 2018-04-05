49ddfb975f8466bb9aff2c6d8a946d4a54ffbd52
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-05 15:52:10 +0300

    Update lang_frequency.py
    исправлена работа функций и названия переменных

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,24 +5,24 @@ import sys
 
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
-        content = file.read().lower()
+        content = file.read()
         return content
 
 
-def get_most_frequent_words(text):
-    all_words_in_text = collections.Counter(re.findall(r'\w+', text))
-    number_of_words_for_output = 10
-    most_frequent_words = all_words_in_text.most_common(number_of_words_for_output)
-    return most_frequent_words
+def get_most_frequent_words(text, num_of_words=10):
+    return collections.Counter(re.findall(r'\w+', text.lower())).most_common(num_of_words)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        exit("Need input file path as parameter")
+        exit('Need input file path as parameter')
     try:
-        most_frequent_words_lst = get_most_frequent_words(load_data(sys.argv[1]))
-        for words_and_counts in most_frequent_words_lst:
-            print(words_and_counts[0], ": ", words_and_counts[1], sep='')
-    except FileNotFoundError:
-        exit("File not found")
+        top_ten_words = get_most_frequent_words(load_data(sys.argv[1]))
+        print('The most common words in the text:')
+        for element in top_ten_words:
+            word = element[0]
+            count = element[1]
+            print(word, count)
 
+    except FileNotFoundError:
+        exit('File not found')
