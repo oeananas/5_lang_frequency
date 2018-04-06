5438c9987ff0c8cc096f8dfa144973fd1ec19c44
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-06 20:14:05 +0300

    Update lang_frequency.py
    Исправлена работа блока исключений

--- lang_frequency.py
+++ lang_frequency.py
@@ -9,7 +9,7 @@ def load_data(file_path):
         return content
 
 
-def get_count_words(text):
+def get_most_frequent_words(text):
     return collections.Counter(re.findall(r'\w+', text.lower()))
 
 
@@ -17,11 +17,10 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         exit('Need input file path as parameter')
     try:
-        most_frequent_words = get_count_words(load_data(sys.argv[1]))
-        num_of_words = 10
-        print('The most common words in the text:')
-        for word, count in most_frequent_words.most_common(num_of_words):
-            print(word, count)
-
+        most_frequent_words = get_most_frequent_words(load_data(sys.argv[1]))
     except FileNotFoundError:
         exit('File not found')
+    num_of_words = 10
+    print('The most common words in the text:')
+    for word, count in most_frequent_words.most_common(num_of_words):
+        print(word, count)
